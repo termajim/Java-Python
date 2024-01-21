efc8c6dc6639f2a56e3324ac96077c9f55e7fd71
termajim <jim.vapola@metropolia.fi>
2024-01-21 16:56:22 +0200

    Cc

---
+++ mod02/04Kokonaisluvut.py
@@ -0,0 +1,15 @@
+#Kokonaisluvut ohjelma
+
+luku1 = float(input("Esitä ensimmäinen kokonaisluku: "))
+luku2 = float(input("Esitä toinen kokonaisluku: "))
+luku3 = float(input("Esitä kolmas kokonaisluku: "))
+
+#Lukujen arvot
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print("Summa:", summa)
+print("Tulo:", tulo)
+print("Keskiarvo:", keskiarvo)
